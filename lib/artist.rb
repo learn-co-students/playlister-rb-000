f2cd6459091cfc17f0034ecb23c2c011fb0f7914
aeremin <aeremin@maxus.ru>
2015-07-08 13:52:33 +0300

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,25 @@
 # code artist here
+class Artist
+  @@all = []
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.size
+  end	
+  def self.reset_artists
+    @@all.clear
+  end
+
+  attr_accessor :name, :songs, :genres
+  def initialize
+    @songs = []
+	@genres = []
+	@@all << self
+  end	
+  def add_song(song)
+    @songs << song unless @songs.include?(song)
+    @genres << song.genre unless @genres.include?(song.genre)
+	song.genre.artists << self unless ( song.genre == nil || song.genre.artists.include?(self) )
+  end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,23 @@
 # code genre here
+class Genre
+  @@all = []
+  def self.reset_genres
+    @@all.clear
+  end	
+  
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end	
+  
+  attr_reader :songs, :artists
+  attr_accessor :name
+  def initialize 
+    @songs = []
+	@artists = []
+	@@all << self
+  end
+end
