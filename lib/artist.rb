411884482222fd13ac04ce9c4ba745ec36b9cab3
NoneOfMaster <rcraigt@gmail.com>
2015-10-25 19:17:49 -0700

    funny playlist pun

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,33 @@
-# code artist here
+class Artist
+
+  @@artists = [] 
+
+  attr_accessor :songs, :name
+
+  def initialize
+    @songs = []
+    @@artists << self
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+  end
+
+  def genres
+    self.songs.collect{|song| song.genre}
+  end
+
+  def self.reset_artists
+    @@artists = []
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def self.count
+    @@artists.count
+  end
+
+end
