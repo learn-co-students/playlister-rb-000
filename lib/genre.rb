95fe4078688ba9a2da1e9a59b530ab706b6935a7
pkprosol <peter.prosol@gmail.com>
2017-02-28 20:35:44 -0500

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,29 @@
 # code genre here
+class Genre
+    @@genres = []
+    
+    attr_accessor :songs, :name
+
+    def initialize
+        @songs = []
+        @@genres << self
+    end
+
+    def artists
+        @songs.collect do |song|
+            song.artist
+        end.uniq
+    end
+
+    def self.reset_genres
+        @@genres = []
+    end
+
+    def self.all
+        @@genres
+    end
+
+    def self.count
+        @@genres.count
+    end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,9 @@
 # code song here
+class Song
+    attr_accessor :artist, :genre, :name
+
+    def genre=(genre)
+        @genre = genre
+        genre.songs << self
+    end
+end
