9add8851a21683eeec9f2c3ef27465339c573320
OnlyHughMan <hughvidler@hotmail.com>
2017-06-23 07:11:36 +1000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,29 @@
-# code genre here
+class Genre
+	attr_accessor :name, :artists, :songs
+	@@genres = []
+	def initialize
+		@@genres << self
+		@songs = []
+		@artists = []
+	end
+
+	def artists
+		self.songs.each do |s|
+			@artists << s.artist
+		end
+		@artists | @artists
+	end
+
+	def self.reset_genres
+		@@genres = []
+	end
+
+	def self.all
+		@@genres
+	end
+
+	def self.count
+		@@genres.uniq.count
+	end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,13 @@
-# code song here
+class Song
+	attr_accessor :name, :genre, :artist
+	@@songs = []
+	def initialize
+		@@songs << self
+	end
+
+	def genre=(genre)
+ 		@genre = genre
+ 		genre.songs << self
+ 	end
+
+end
